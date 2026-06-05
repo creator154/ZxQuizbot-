6f3598a04b3ef1884a2c9adfbb3b1d99478635ff
creator154 <cteator898@gmail.com>
2026-06-05 22:07:59 +0530

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,15 @@
+from random import shuffle
+
+
+def shuffle_questions(data):
+
+    shuffle(data)
+
+    return data
+
+
+def shuffle_answers(options):
+
+    shuffle(options)
+
+    return options
